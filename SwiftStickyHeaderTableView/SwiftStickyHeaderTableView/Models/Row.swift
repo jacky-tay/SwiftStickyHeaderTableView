620ccbdfc51c72d246daa2143fd249e340dae6ab
Jacky Tay <chianghau86@hotmail.com>
2018-06-01 09:09:39 +1200

    Add documentation for Row and Section classes

--- SwiftStickyHeaderTableView/SwiftStickyHeaderTableView/Models/Row.swift
+++ SwiftStickyHeaderTableView/SwiftStickyHeaderTableView/Models/Row.swift
@@ -8,11 +8,6 @@
 
 import Foundation
 
-protocol RowType {
-    var type: CellType { get }
-    var cellIdentifier: String { get }
-}
-
 class Row {
     var cellIdentifier: String { get { return "" } }
     var title: String!
@@ -40,39 +35,64 @@ class Row {
         }
     }
 
+
+    /// Check that child is one of the child of Row object
+    ///
+    /// - Parameter child: The child Row object
+    /// - Returns: `True` if Row object contains child, otherwise `False`
     func has(child: Row) -> Bool {
         return hasChildren() && (children?.contains(where: { $0 == child }) ?? false)
     }
 
+    /// Check that if the Row object has children and it is not empty
+    ///
+    /// - Returns: `True` if Row object has children, otherwise `False`
     func hasChildren() -> Bool {
         return !(children?.isEmpty ?? true)
     }
 
+    /// A recursive method that check the total number of rows a Row object has. A Row object itself is counted as 1.
+    ///
+    /// - Returns: The total number of rows a Row object has.
     func numberOfFlattenRows() -> Int {
         return 1 + (children?.reduce(0) { $0 + $1.numberOfFlattenRows() } ?? 0)
     }
 
+
+    /// Find the flatten Row object from a given index. Row object may contain child which itself may contain child.
+    ///
+    /// - Parameter index: The index
+    /// - Returns: The Row object which located in a flatten dimension.
     func get(flattenRowAt index: Int) -> Row {
         guard index > 0 else {
-            return self
+            return self // return Row object itself if the index is 0
         }
         // dig further
         guard let children = children, children.count > 0 else {
-            return self
+            return self // return Row object itself if there it has no children, this should not happen
         }
         var row = 0
-        var offset = 1
+        var offset = 1 // use offset as reference point when viewing children in a flatten dimension
+        // increment row count when the number of flatten rows is smaller than the index
         for i in 1 ..< children.count where offset < index { // need to subtract 1 because this cell is
             offset += children[i - 1].numberOfFlattenRows()
             row += 1
         }
-        if children[row].hasChildren() && (index - offset) < 0 {
-            offset -= children[row - 1].numberOfFlattenRows()
+        // decrement the row if the found child Row object has children and the index is smaller than the offset reference
+        if row > 0 && children[row].hasChildren() && index < offset  {
             row -= 1
+            offset -= children[row].numberOfFlattenRows()
         }
         return children[row].get(flattenRowAt: index - offset)
     }
 
+
+    /// Check that Row object is identical object
+    ///
+    /// - Parameters:
+    ///   - lhs: The Row object
+    ///   - rhs: The Row object to be compared
+    /// - Returns: `True` if the cell identifier, title, and title are identical, otherwise `False`
     static func ==(lhs: Row, rhs: Row) -> Bool {
         return lhs.cellIdentifier == rhs.cellIdentifier &&
         lhs.detail == rhs.detail &&
--- SwiftStickyHeaderTableView/SwiftStickyHeaderTableView/Models/Section.swift
+++ SwiftStickyHeaderTableView/SwiftStickyHeaderTableView/Models/Section.swift
@@ -21,19 +21,29 @@ class Section {
         footer = json["footer"] as? String
     }
 
+
+    /// Calculate the total numbers of Row object in a flatten dimension
+    ///
+    /// - Returns: The total numbers of Row object
     func numberOfFlattenRows() -> Int {
         return rows.reduce(0) { $0 + $1.numberOfFlattenRows() }
     }
 
+
+    /// Find the Row object in a flatten dimension
+    ///
+    /// - Parameter index: The index
+    /// - Returns: The Row object
     func get(flattenRowAt index: Int) -> Row {
         var row = 0
-        var offset = 0
-
+        var offset = 0 // use offset as reference point when viewing children in a flatten dimension
+        // increment row count when the number of flatten rows is smaller than the index
         for i in 1 ..< rows.count where offset < index {
             offset += rows[i - 1].numberOfFlattenRows()
             row += 1
         }
-        if offset > index {
+        // decrement the row if the index is smaller than the offset reference
+        if row > 0 && index < offset {
             row -= 1
             offset -= rows[row].numberOfFlattenRows()
         }
